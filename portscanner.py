d49054d120712306ae3354e8f4169ae2313fbfe5
CryptAdvance <114677358+CryptAdvance@users.noreply.github.com>
2022-11-14 11:23:33 -0500

    port scanner changes

--- portscanner.py
+++ portscanner.py
@@ -3,41 +3,18 @@ class PortScanner():
 	def POrtScanner(self,input_from_user):
 		r=1
 		while r==1:
-			#a=input("Enter the IP Address for which you want to check if the port is open: \n")
-			#b=input("Enter the port number for which you want to check if it is open or not: ")
-			def connectionScan(targetHost, targetPort):
-				try:
-					connect_t=socket(AF_INET,SOCK_STREAM)
-					connect_t.connect((targetHost, targetPort))
-					print('[+] %d/tcp open'% targetPort)
-					connect_t.close()
-					with open('portstatus.txt','w') as file:
-						file.writelines(str(targetPort)+"/tcp is open \n")
-					
-				except:
-					print('[-] %d/tcp closed'% targetPort)
-					connect_t.close()
-					with open('portstatus.txt','w') as file:
-						file.writelines(str(targetPort)+"/tcp is closed\n")
-			r=2
-				
-			def portScan(targetHost, targetPorts):
-				try:
-					targetIP=gethostbyname(targetHost)
-				except:
-					print("[-] Cannot resolve %s"% targetHost)
-					return
-				try:
-					targetName=gethostbyaddr(targetIP)
-					print('\n[+] Scan result of: %s '% targetName[0])
-				except:
-					print("\n[+] Scan result of: %s "% targetIP)
-				setdefaulttimeout(1)
-				for targetPort in targetPorts:
-					print("Scanning Port: %d "% int(targetPort))
-					connectionScan(targetHost, int(targetPort))
-			portScan("google.com",[80,22])
+			import socket
+			s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.settimeout(5)
+			a=input("Enter the IP Address for which you want to check if the port is open: \n")
+			b=int(input("Enter the port number for which you want to check if it is open or not: "))
+			def portSCanner(b):
+				if s.connect_ex((a,b)):
+					print("The port is closed")
+				else:
+					print("The port is open")
+			portSCanner(b)
+			
 
 		else:
 			r=2
-#https://www.youtube.com/watch?v=bH-3PuQC_n0&list=PLR0bgGon_WTIjs0lyCAUp3v1qAraXCJcH
